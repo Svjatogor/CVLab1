fd401579114618553d9fba71c99f7d7ead832fd1
Timofey <vsjo.zanjato@gmail.com>
2017-02-12 19:55:44 +0300

    Added threads

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-import cv2 as cv
-from PyQt4 import QtGui, QtCore
-
 # -*- coding: utf-8 -*-
 
-import cv2
 import sys
 from PyQt4 import QtGui, QtCore
+import threading
+import cv2 as cv
+from os.path import expanduser
+
+class ImageTransformThread(threading.Thread):
+    def __init__(self, image_path):
+        threading.Thread.__init__(self)
+        self.image_path = image_path
+
+    def run(self):
+        while True:
+            pass
+
+    def negative_roi(self, str_filename):
+        img = cv.imread(str_filename)
+        window_name = str_filename.split('/')[-1].split('.')[0]
+        cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
+        # set callback to click mouse
+        cv.setMouseCallback(window_name, self.my_mouse_callback, img)
+        while True:
+            cv.imshow(window_name, img)
+
+            key = cv.waitKey(33)
+            if key == 27:
+                break
+        cv.destroyWindow(window_name)
 
 
 class MainApp(QtGui.QWidget):
@@ -31,21 +53,13 @@ class MainApp(QtGui.QWidget):
         self.open_btn.clicked.connect(self.open_image)
 
     def open_image(self):
-        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open image', '/home/', "Image files (*.jpg *.png)")
+        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open image', expanduser("~"), "Image files (*.jpg *.png)")
         if not filename.isEmpty():
             str_filename = filename.toUtf8().data()
-            img = cv.imread(str_filename)
-            window_name = str_filename.split('/')[-1].split('.')[0]
-            cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
-            # set callback to click mouse
-            cv.setMouseCallback(window_name, self.my_mouse_callback, img)
-            while True:
-                cv.imshow(window_name, img)
-
-                key = cv.waitKey(33);
-                if key == 27:
-                    break
-            cv.destroyWindow(window_name)
+            # create thread
+            # t = threading.Thread(target=self.negative_roi, args=(str_filename,))
+            # t.start()
+            self.negative_roi(str_filename)
 
     def my_mouse_callback(self, event, x, y, flags, param):
         if event == cv.EVENT_LBUTTONDOWN:
