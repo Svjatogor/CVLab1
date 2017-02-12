f1b78532b091ded4ead488571a15caa0431b4451
Timofey <vsjo.zanjato@gmail.com>
2017-02-12 22:59:06 +0300

    Added negative one selection

--- negative_image.py
+++ negative_image.py
@@ -8,10 +8,10 @@ class NegativeImage:
         self.image_path = image_path
         self.left_press = False
         self.start_point = ()
+        self.end_point = ()
         self.window_name = self.image_path.split('/')[-1].split('.')[0]
         self.img = cv.imread(self.image_path)
         self.origin = copy.copy(self.img)
-        self.neg_image = copy.copy(self.img)
 
     def my_mouse_callback(self, event, x, y, flags, param):
         if event == cv.EVENT_LBUTTONDOWN:
@@ -19,9 +19,31 @@ class NegativeImage:
             self.start_point = (x, y)
         elif event == cv.EVENT_LBUTTONUP:
             self.left_press = False
+            # discover start and end points
+            start_rect = []
+            end_rect = []
+            if self.start_point[0] > x:
+                start_rect.append(x)
+                end_rect.append(self.start_point[0])
+            else:
+                start_rect.append(self.start_point[0])
+                end_rect.append(x)
+
+            if self.start_point[1] > y:
+                start_rect.append(self.end_point[1])
+                end_rect.append(self.start_point[1])
+            else:
+                start_rect.append(self.start_point[1])
+                end_rect.append(self.end_point[1])
+
+            
+            self.img[start_rect[1]:end_rect[1], start_rect[0]:end_rect[0]] = \
+                255 - self.img[start_rect[1]:end_rect[1], start_rect[0]:end_rect[0]]
+
         elif event == cv.EVENT_MOUSEMOVE:
             if self.left_press:
                 self.img = copy.copy(self.origin)
+                self.end_point = (x, y)
                 cv.rectangle(self.img, self.start_point, (x, y), (255, 153, 0))
 
     def get_negative_roi(self):
